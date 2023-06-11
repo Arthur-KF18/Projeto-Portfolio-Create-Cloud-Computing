a5d31bf743ee60e8c63ddcb78c943fcde721dd26
Arthur Korkiskis Felismino <arthurkfelismino@gmail.com>
2023-06-11 20:35:21 -0300

    adicionando swiper ao main.js

--- Assets/static/main.js
+++ Assets/static/main.js
@@ -2,3 +2,18 @@ window.scroll({
     top: 0,
     behavior: 'smooth',
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      breakpoints: {
+        1192: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }
+    }
+  });
